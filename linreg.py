b9477203f9263841e095051a7e6fed6369ea8f46
Dave Beck <dacb@uw.edu>
2016-04-15 13:37:35 -0700

    step 1 plotting

--- linreg.py
+++ linreg.py
@@ -2,10 +2,15 @@
 
 # imports
 #import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
 from sklearn.datasets import load_boston
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from statsmodels.graphics.regressionplots import abline_plot
+
+PDF='linreg.pdf'
 
 # migrate sklearn dict data to DF
 boston = load_boston()
@@ -22,3 +27,12 @@ bostonDF['MEDV'] = boston.target
 results = smf.ols('MEDV ~ LSTAT', data=bostonDF).fit()
 print(results.summary())
 
+# plot
+#plt.plot()
+with PdfPages(PDF) as pdf:
+    fig = abline_plot(model_results=results)
+    ax = fig.axes[0]
+    ax.scatter(bostonDF['LSTAT'], bostonDF['MEDV'])
+    ax.margins(.1)
+    pdf.savefig()
+    plt.close()
